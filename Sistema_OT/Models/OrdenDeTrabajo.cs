0684d63ca04f3507ed0710ef9017f24d93acdf05
Fantino-Russo <fantinojavierrusso@gmail.com>
2024-10-18 11:29:18 -0300

    Agregando todos los campos de la base de datos para la clase orden de trabajo

--- Sistema_OT/Models/OrdenDeTrabajo.cs
+++ Sistema_OT/Models/OrdenDeTrabajo.cs
@@ -6,13 +6,28 @@ namespace Sistema_OT.Models
 {
     public class OrdenDeTrabajo
     {
+
+        
         public decimal NroOrdenTrabajo { get; set; }
         public int Cliente { get; set; }
         public int Sistema { get; set; }
+        public string Modulo {  get; set; }
         public string Asunto { get; set; }
-        public DateTime FechaSolicitud { set; get; }
+        public DateTime FechaSolicitud { get; set; }
+        public DateTime FechaFinalizacion { get; set; }
+        public int CantidadHorasEstimadas {  get; set; }
+        public int CantidadHorasConsumidas {  get; set; }
+        public int Estado {  get; set; }
+        public int PorcentajeAvance {  get; set; }
+        public int UsuarioSolicitante {  get; set; }
+        public int UsuarioResponsable {  get; set; }
+        public string Descripcion {  get; set; }
+        public string Observaciones { get; set; }
+        public int Prioridad { get; set; }
+        public string FormulariosModificados { get; set; }
+        public string ModificacionesBaseDatos { get; set; }
         public string UserIDSolicitante { get; set; }
-
+        public string UserIDResponsable { get; set; }
         public static List<OrdenDeTrabajo> ObtenerLista(int num)
         {
             string consulta = "Select * from Ordenes_Trabajo Where NroOrdenTrabajo = @numero";
@@ -32,12 +47,28 @@ namespace Sistema_OT.Models
                         {
                             OrdenDeTrabajo Orden = new OrdenDeTrabajo
                             {
+
+                                
                                 NroOrdenTrabajo = reader.GetDecimal(reader.GetOrdinal("NroOrdenTrabajo")),
                                 Cliente = reader.GetInt32(reader.GetOrdinal("Cliente")),
                                 Sistema = reader.GetInt32(reader.GetOrdinal("Sistema")),
+                                Modulo = reader.GetString(reader.GetOrdinal("Modulo")),
                                 Asunto = reader.GetString(reader.GetOrdinal("Asunto")),
                                 FechaSolicitud = reader.GetDateTime(reader.GetOrdinal("FechaSolicitud")),
-                                UserIDSolicitante = reader.GetString(reader.GetOrdinal("UserIDSolicitante"))
+                                FechaFinalizacion = reader.GetDateTime(reader.GetOrdinal("FechaFinalizacion")),
+                                CantidadHorasEstimadas = reader.GetInt32(reader.GetOrdinal("CantidadHorasEstimadas")),
+                                CantidadHorasConsumidas = reader.GetInt32(reader.GetOrdinal("CantidadHorasConsumidas")),
+                                Estado = reader.GetInt32(reader.GetOrdinal("Estado")),
+                                PorcentajeAvance = reader.GetInt32(reader.GetOrdinal("PorcentajeAvance")),
+                                UsuarioSolicitante = reader.GetInt32(reader.GetOrdinal("UsuarioSolicitante")),
+                                UsuarioResponsable = reader.GetInt32(reader.GetOrdinal("UsuarioResponsable")),
+                                Descripcion = reader.GetString(reader.GetOrdinal("Descripcion")),
+                                Observaciones = reader.GetString(reader.GetOrdinal("Observaciones")),
+                                Prioridad = reader.GetInt32(reader.GetOrdinal("Prioridad")),
+                                FormulariosModificados = reader.GetString(reader.GetOrdinal("FormulariosModificados")),
+                                ModificacionesBaseDatos = reader.GetString(reader.GetOrdinal("ModificacionesBaseDatos")),
+                                UserIDSolicitante = reader.GetString(reader.GetOrdinal("UserIDSolicitante")),
+                                UserIDResponsable = reader.GetString(reader.GetOrdinal("UserIDResponsable"))
                             };
                             OrdenesTrabajo.Add(Orden);
                         }
