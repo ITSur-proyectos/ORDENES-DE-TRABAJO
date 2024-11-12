ada9a309b47f2d39d4ada427ee3981e70fea65d4
terrazfacundo <terrazfacundo06@gmail.com>
2024-11-12 12:52:07 -0300

    Cambio en Filecontroller, guardar archivo

--- Sistema_OT/Controllers/FilesController.cs
+++ Sistema_OT/Controllers/FilesController.cs
@@ -1,10 +1,38 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace Sistema_OT.Controllers
 {
     public class FilesController : Controller
     {
-         
+        [HttpPost]
+        public async Task<IActionResult> UploadFile(IFormFile fileUpload)
+        {
+            if (fileUpload != null && fileUpload.Length > 0)
+            {
+                
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "uploads", fileUpload.FileName);
+
+                
+                if (!Directory.Exists(Path.GetDirectoryName(path)))
+                {
+                    Directory.CreateDirectory(Path.GetDirectoryName(path));
+                }
+
+               
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    await fileUpload.CopyToAsync(stream);
+                }
+
+               
+                return RedirectToAction("VistaIndividual", "Home");
+            }
+
+            
+            return View("Error"); 
+        }
     }
 }
