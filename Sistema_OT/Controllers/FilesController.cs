c4033e834433d827ef871dc6569caa07c4a52d9b
Abril <sepulveda.abril.epet20@gmail.com>
2024-11-13 10:16:31 -0300

    Agregando en Files.controller código para subir varios archivos a la vez.

--- Sistema_OT/Controllers/FilesController.cs
+++ Sistema_OT/Controllers/FilesController.cs
@@ -1,10 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace Sistema_OT.Controllers
 {
     public class FilesController : Controller
     {
-         
+        [HttpPost("upload")]
+        public async Task<IActionResult> UploadFiles(List<IFormFile> files)
+        {
+
+
+        }
     }
 }
