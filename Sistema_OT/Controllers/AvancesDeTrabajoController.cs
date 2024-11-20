48dca82a0a1c71743ac902b90d7ca091683106d2
terrazfacundo <terrazfacundo06@gmail.com>
2024-11-20 13:01:48 -0300

    AvancesDeTrabajoController

---
+++ Sistema_OT/Controllers/AvancesDeTrabajoController.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Sistema_OT.Models;
+using System.Data.SqlClient;
+
+namespace Sistema_OT.Controllers
+{
+    public class AvancesDeTrabajoController : Controller
+
+    {
+        private readonly string _connectionString = "Data Source=192.168.110.5;Initial Catalog=Db_ITSur_CSharp;User ID=sa;Password=felisa5";
+
+        // GET: AvancesDeTrabajoController
+        public ActionResult Index()
+        {
+            return View("VistaIndividual");
+
+        }
+
+        private List<AvanceDeTrabajo> GetAllAvances()
+        {
+            //cambiar el nombre del Model
+            var avances = new List<AvanceDeTrabajo>();
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                var query = "select * from Avances_Trabajo";
+                using (var command = new SqlCommand(query, connetion)) ;
+                {
+                    connection.Open();
+                    using (var reader = commnad.ExecuteReader())
+                    {
+        {
+                       
+                    }
+            }
+        }        
+
+
+        // GET: AvancesDeTrabajoController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: AvancesDeTrabajoController/Create
+        public ActionResult Create()
+        {
+            Console.WriteLine("Estoy en el controlador");
+            return View();
+        }
+
+        // POST: AvancesDeTrabajoController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AvancesDeTrabajoController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: AvancesDeTrabajoController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AvancesDeTrabajoController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: AvancesDeTrabajoController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Sistema_OT/Models/AvanceDeTrabajo.cs
@@ -0,0 +1,33 @@
+﻿namespace Sistema_OT.Models
+{
+    public class AvanceDeTrabajo
+    {
+        public decimal NroOrdenTrabajo { get; set; }
+
+        public int AvanceTrabajo { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public DateTime Fecha { get; set; }
+
+        public decimal HorasInsumidas { get; set; }
+
+        public char Terminado { get; set; }
+
+        public DateTime FechaAlta { get; set; }
+
+        public string UserIDAlta { get; set; }
+
+        public DateTime FechaUltimaModificacion { get; set; }
+
+        public string UserIDModificacion { get; set; }
+
+        public DateTime FechaBaja { get; set; }
+
+        public string UserIDBaja { get; set; }
+
+        public int Usuario { get; set; }
+
+        public int TipoAvance { get; set; }
+    }
+}
